c8b21e0d774174975c74601d7decd1ea12fd051a
paulinr <paulin.ralph@yahoo.fr>
2018-04-15 18:36:44 -0500

    dernier mis-a-jour du dimanche 15 avril

--- TPFLEURIMEMICHELPAULIN-ejb/src/java/session/GestionnaireDeCompteBancaire.java
+++ TPFLEURIMEMICHELPAULIN-ejb/src/java/session/GestionnaireDeCompteBancaire.java
@@ -5,41 +5,158 @@
  */
 package session;
 
+import entities.Client;
 import entities.CompteBancaire;
+import entities.TransactionBancaire;
+import java.io.Serializable;
 import java.util.List;
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
 import javax.ejb.LocalBean;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.persistence.Query;
+
 
 /**
  *
  * @author dmichel
  */
+//@TransactionManagement(TransactionManagementType.BEAN) 
 @Stateless
 @LocalBean
-public class GestionnaireDeCompteBancaire {
+public class GestionnaireDeCompteBancaire implements Serializable {
 
     // Add business logic below. (Right-click in editor and choose
     // "Insert Code > Add Business Method")
-    @PersistenceContext(unitName = "TPFLEURIMEMICHELPAULIN-ejbPU")
+    //@Resource 
+   // UserTransaction ut; 
+    
+    @PersistenceContext //(unitName = "TPFLEURIMEMICHELPAULIN-ejbPU")
     private EntityManager em;
 
     public void creerCompte(CompteBancaire compteBancaire) {
-        em.persist(compteBancaire);
+        
+       // try {
+           //  ut.begin();
+             em.persist(compteBancaire);
+            // ut.commit();
+            
+//        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException ex) {
+//            try {
+//                ut.rollback();
+//            } catch (IllegalStateException | SecurityException | SystemException e) {
+//                throw new RuntimeException(ex);
+//            }
+   //     }
+       
     }
 
     public List<CompteBancaire> getAllComptes() {
         Query query = em.createNamedQuery("CompteBancaire.findAll");
         return query.getResultList();
     }
-
-    public void creerComptesTest() {
-        creerCompte(new CompteBancaire("Paulin", 150000));
-        creerCompte(new CompteBancaire("Fleurime", 950000));
-        creerCompte(new CompteBancaire("Michel", 20000));
+    
+     public List<CompteBancaire> getComptesByClient(Long clientId) {
+        Query query = em.createNamedQuery("CompteBancaire.findCompteByClientId");
+        query.setParameter("clientId", clientId);
+        return query.getResultList();
     }
+     
+     public List<CompteBancaire> getLazyCptes(int start, int nbComptes){
+        
+        Query query =em.createNamedQuery("CompteBancaire.findAll");
+        query.setFirstResult(start);
+        query.setMaxResults(nbComptes);
+        
+        return query.getResultList();
+    }
+    
+  public int getNbComptes(){
+        Query query = em.createNamedQuery("CompteBancaire.nbComptes");
+        
+        return ((Long) query.getSingleResult()).intValue();
+    }
+    
+    /**
+     * Supprimer un compte bancaire
+     * @param compte 
+     */
+    public void delete(CompteBancaire compte) {
+        // try {
+            
+          //   ut.begin();
+             em.remove(em.merge(compte));
+         //    ut.commit();
+//        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException ex) {
+//             try {
+//                   ut.rollback();
+//             } catch (IllegalStateException | SecurityException | SystemException e) {
+//                  throw new RuntimeException(ex);
+//             }
+//            
+//        }
+        
+        
+    }
+    
+    public void creerOperation(CompteBancaire compte, String descrption, float montant){
+        TransactionBancaire tb = new TransactionBancaire(montant, descrption);
+        compte.getTransactions().add(tb);
+    }
+    
+    public CompteBancaire Consulter(String noCompte) { 
+        Query query= em.createNamedQuery("CompteBancaire.findByCompteNoCpte");
+        return (CompteBancaire) query.getResultList();
+    }
+
+  
+    public CompteBancaire crediterCompte(CompteBancaire compte, float montant){
+      // try {
+          //  ut.begin();
+            compte.deposer(montant);
+            creerOperation(compte, "Dépot", montant);
+            em.merge(compte);
+           // ut.commit();
+//            System.out.println("DEPOT REUSSI");
+//        } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException ex) {
+//            try {
+//                ut.rollback();
+//            } catch (IllegalStateException | SecurityException | SystemException e) {
+//               throw new RuntimeException(ex); 
+//            }
+//        }
+        return update (compte);
+        
+    }
+    
+    public CompteBancaire debiterCompte(CompteBancaire compte, float montant){
+        compte.retirer(montant);
+        creerOperation(compte, "Retrait", montant);
+        em.merge(compte);
+        System.out.println("RETRAIT REUSSI");
+        return update (compte);
+    }
+    
+    
+    public void transaction(int tb, CompteBancaire compte, float montant){
+               
+        if (tb == 0){
+            compte = this.debiterCompte(compte, montant);
+        }
+        else 
+            if (tb == 1) {
+            compte= this.crediterCompte(compte, montant);      
+            }
+           
+        
+        FacesMessage message = new FacesMessage(FacesMessage.SEVERITY_INFO, "Opération réussie !","La transaction a été effectuée");  
+          
+        FacesContext.getCurrentInstance().addMessage(null, message);
+    }
+ 
 
     public CompteBancaire update(CompteBancaire comptebancaire) {
         return em.merge(comptebancaire);
@@ -49,7 +166,52 @@ public class GestionnaireDeCompteBancaire {
         em.persist(compteBancaire);
     }
 
-    public CompteBancaire getCompteBancaire(Long idCompteBancaire) {
-        return em.find(CompteBancaire.class, idCompteBancaire);
+    public CompteBancaire getCompteBancaire(Long idCompte) {
+        return em.find(CompteBancaire.class,idCompte );
     }
+    
+     public void virement(Long idDebiteur, Long idCrediteur, float montant){
+//         try {
+             //ut.begin();
+             System.out.println("==========================");
+             System.out.println("idDebiteur: "+idDebiteur);
+             System.out.println("idCrediteur: "+idCrediteur);
+             System.out.println("Montant: "+montant);
+             System.out.println("==========================");
+        CompteBancaire compteDebiteur = getCompteBancaire(idDebiteur);// getCompteBancaire(idDebiteur);
+        CompteBancaire compteCrediteur= getCompteBancaire(idCrediteur);
+        
+       //compteDebiteur.retirer(montant);
+       //compteCrediteur.deposer(montant);
+        
+        debiterCompte(compteDebiteur, montant);
+        crediterCompte(compteCrediteur, montant);
+        
+//        creerOperation(compteDebiteur, "Virement effectué à "+compteCrediteur.getClient().getNom()+"-"+compteCrediteur.getClient().getPrenom(), montant);
+//        creerOperation(compteCrediteur, "Virement reçu de "+ compteDebiteur.getClient().getNom()+"-"+compteDebiteur.getClient().getPrenom(), montant);
+//        
+//       em.merge(compteDebiteur);
+//        em.merge(compteCrediteur); 
+//            ut.commit();
+//         } catch (IllegalStateException | SecurityException | HeuristicMixedException | HeuristicRollbackException | NotSupportedException | RollbackException | SystemException ex) {
+//             try {
+//                 ut.rollback();
+//                 
+//             } catch (IllegalStateException | SecurityException | SystemException e) {
+//                 throw new RuntimeException(ex); 
+//             }
+//         }
+        
+       
+     }
+     
+     public List<CompteBancaire> getCompteByCliId(Long clientId) {
+        Query query = em.createNamedQuery("CompteBancaire.findCompteByClientId").setParameter("clientId", clientId);
+        return query.getResultList();
+    }
+     public void creerComptesTest() {  
+         Client client= new Client();
+         CompteBancaire cpte= new CompteBancaire();
+     }
+    
 }
